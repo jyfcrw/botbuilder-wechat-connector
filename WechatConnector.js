5470dc6f3ab4767a6785712a53254249b495c8d0
jyfcrw <jyfcrw@gmail.com>
2016-08-26 17:50:35 +0800

    add main script

---
+++ WechatConnector.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var botframework = require('botbuilder');
+var WechatApi = require('wechat-api');
+var wechat = require('wechat');
+
+var WechatConnector = (function() {
+    function WechatConnector(options) {
+        // todo
+    }
+
+    return WechatConnector;
+}());
+
+exports.WechatConnector = WechatConnector;
